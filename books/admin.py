7d4baae502faa5b52f52647444b97bb39b70aa41
Ali <Alihunter4640@gmail.com>
2024-06-30 20:45:44 +0330

    book app models made

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import * 
 
-# Register your models here.
+
+
+class BookTitleAdmin(admin.ModelAdmin):
+    list_display = ['title','author','publisher'] 
+    list_filter = ['author','publisher']
+    date_hierarchy = 'created_at'
+
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['book_title']
+    list_filter = ['book_title']
+    date_hierarchy = 'created_at'
+
+
+admin.site.register(BookTitle,BookTitleAdmin)
+admin.site.register(Book,BookAdmin)
+
+
