951e4f5c0ea77bcf8d694997b93a1c18280479c5
Ali <Alihunter4640@gmail.com>
2024-07-06 14:26:29 +0330

    new functionality for clean method
    that checks that the book title don't be duplicate and if already exists in model raise and validation exception

--- books/forms.py
+++ books/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from .models import * 
-
+from django.core.exceptions import ValidationError
 
 class BookTitleForm(forms.ModelForm):
     class Meta:
@@ -12,5 +12,11 @@ class BookTitleForm(forms.ModelForm):
         
         if len(title) < 5:
             error_msg = 'Your Book title should be 5 characters at least!'
-            raise forms.ValidationError(error_msg)
+            raise ValidationError(error_msg)
+    
+        book_title_exist = BookTitle.objects.filter(title__iexact=title).exists()
+        if book_title_exist:
+            duplicate_error_msg = 'Book with this title already exists'
+            raise ValidationError(duplicate_error_msg)
+        
         return self.cleaned_data
