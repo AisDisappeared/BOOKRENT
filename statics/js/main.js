46b2a0db3f265f7a2b7f48fecd92650cedf57358
Ali <Alihunter4640@gmail.com>
2024-07-07 16:27:34 +0330

    javascript files added
    place form in a modal + adding js to the modal and fixing the form

---
+++ statics/js/main.js
@@ -0,0 +1,29 @@
+// creating variables
+const formModal = document.getElementById('form-modal')
+const cancelbtn = document.getElementById('cancel-btn') 
+const openModalBtn = document.getElementById('open-modal-btn')
+const backdrop = document.getElementById('backdrop')
+
+
+
+// printing the variables
+console.log(formModal)
+console.log(cancelbtn)
+console.log(backdrop)
+
+
+
+// functions
+openModalBtn.addEventListener('click', ()=> {
+    formModal.classList.remove('hidden')
+})
+
+cancelbtn.addEventListener('click' , ()=> {
+    formModal.classList.add('hidden')
+})
+
+formModal.addEventListener('click',(e)=>{
+    if (e.target !== backdrop) return;
+    console.log(e.target)
+    formModal.classList.add('hidden')
+})
