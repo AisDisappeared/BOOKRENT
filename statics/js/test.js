9b5b634aeda45b7be766e65be0a9b6ca759d56cf
Ali <Alihunter4640@gmail.com>
2024-07-09 14:06:05 +0330

    adding some js code to have clipboard copy functionality

--- statics/js/test.js
+++ statics/js/test.js
@@ -4,3 +4,14 @@ if (head) {
     const backBtn = document.getElementById('go-back-btn')
     backBtn?.addEventListener('click' , ()=> history.back())
 }
+
+
+const copyBtnBox = document.getElementById('copy-box-btn')
+const bookIDforCopy = document.getElementById('book-copy-box')
+
+copyBtnBox.addEventListener('click', ()=>{
+    const bookID = bookIDforCopy.textContent; 
+    navigator.clipboard.writeText(bookID).then(()=> {
+        copyBtnBox.innerHTML = "<p>copied!</p>"
+    })
+})
